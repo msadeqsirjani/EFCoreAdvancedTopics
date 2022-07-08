38011069b9d7edbdf1f9917156426e12bb9ab200
Mohammad Sadeq Sirjani <45625225+msadeqsirjani@users.noreply.github.com>
2022-07-08 12:23:41 +0430

    IsolationLevel

--- EfCoreTopics/Controllers/AdventureWorkController.cs
+++ EfCoreTopics/Controllers/AdventureWorkController.cs
@@ -1,4 +1,5 @@
-﻿using EfCoreTopics.Database;
+﻿using System.Data;
+using EfCoreTopics.Database;
 using EfCoreTopics.Database.Models;
 using EfCoreTopics.Database.ValueObjects;
 using Microsoft.AspNetCore.Mvc;
@@ -155,9 +156,59 @@ public class AdventureWorkController : ControllerBase
     [HttpGet]
     public async Task<IActionResult> GetProductPrices(MoneyType unit)
     {
-        var productPrices = await _context.ProductPrices.AsNoTracking().Where(x=>x.Money.Unit == unit).ToListAsync();
+        var productPrices = await _context.ProductPrices.AsNoTracking().Where(x => x.Money.Unit == unit).ToListAsync();
 
         return Ok(productPrices);
+    }
+
+    #endregion
+
+    #region Isolation Level
+
+    [HttpGet]
+    public async Task<IActionResult> AddSomeProducts()
+    {
+        var transaction = await _context.Database.BeginTransactionAsync(IsolationLevel.ReadUncommitted);
+
+        _context.Database.SetCommandTimeout(TimeSpan.FromSeconds(100));
+
+        try
+        {
+            ProductModel productModelOne = new() { Name = Guid.NewGuid().ToString("D") };
+            ProductModel productModelTwo = new() { Name = Guid.NewGuid().ToString("D") };
+
+            ProductDescription productOneDescription = new() { Description = Guid.Empty.ToString("D") };
+            ProductDescription productTwoDescription = new() { Description = Guid.Empty.ToString("D") };
+
+            await _context.ProductModels.AddRangeAsync(productModelOne, productModelTwo);
+            await _context.ProductDescriptions.AddRangeAsync(productOneDescription, productTwoDescription);
+            await _context.SaveChangesAsync();
+
+            await transaction.CreateSavepointAsync("Save Product & Description");
+
+            await Task.Delay(4_000);
+
+            var savingProductDescriptionCount = await _context.ProductDescriptions.CountAsync();
+
+            ProductDescription productThreeDescription = new() { Description = Guid.Empty.ToString("D") };
+            ProductDescription productFourDescription = new() { Description = Guid.Empty.ToString("D") };
+
+            await _context.ProductDescriptions.AddRangeAsync(productThreeDescription, productFourDescription);
+            await _context.SaveChangesAsync();
+
+            var anotherSavingProductDescriptionCount = await _context.ProductDescriptions.CountAsync();
+
+            if (savingProductDescriptionCount + 2 != anotherSavingProductDescriptionCount)
+                await transaction.RollbackToSavepointAsync("Save Product & Description");
+
+            await transaction.CommitAsync();
+
+            return Ok();
+        }
+        catch(Exception ex)
+        {
+            return Content(ex.Message);
+        }
     }
 
     #endregion
